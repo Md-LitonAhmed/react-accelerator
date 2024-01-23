c6721fa6569b386dff01497b9e473b606f7fc6d5
“liton-ahmed” <litonahmed066@gmail.com>
2024-01-23 19:25:33 +0600

    Previous button is added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import {sculptureList} from './Data'
 function App() {
   const [index, setIndex]=useState(0);
   const [showDetails, setShowDetails]=useState(true)
+  let hasPre= index > 0;
+  let hasNext = index < sculptureList.length - 1;
 
-  const handleClick=()=>{
+  const handleNext=()=>{
     setIndex(index +1 );
+  }
+  const handlePre=()=>{
+    setIndex(index -1 );
   }
   const handleShowDetails=()=>{
     setShowDetails(!showDetails)
@@ -14,7 +19,8 @@ const sculpture = sculptureList[index]
   return (
     <>
       <div>
-        <button onClick={handleClick}>Next</button>
+        <button onClick={handlePre} disabled={!hasPre}>Previous</button>
+        <button onClick={handleNext}  disabled={!hasNext}>Next</button>
         <h2> <i>{sculpture.name}</i> By {sculpture.artist}</h2>
         <h3>{index + 1} of {sculptureList.length}</h3>
         <button onClick={handleShowDetails}>{showDetails ? "Hide": "Show"} Details</button>
